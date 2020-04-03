890403e42345b252bbb30f46ebea37867276393d
nimalikeny07 <59398431+nimalikeny07@users.noreply.github.com>
2020-04-03 16:41:20 +0530

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,25 @@
+import cv2 
+
+cap = cv2.VideoCapture(0)
+ 
+if not (cap.isOpened()):
+
+	print("Could not open video device")
+
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1000);
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600);
+
+while(True):
+
+        ret, frame = cap.read()
+
+        cv2.imshow('preview',frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+
+                break
+
+cap.release()
+
+cv2.destroyAllWindows()
